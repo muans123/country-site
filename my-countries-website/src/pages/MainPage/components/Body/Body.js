dbb2f1ce7e356a122aff05532aa27985a58bf345
muans123 <kiliosgaming06@gmail.com>
2023-06-15 18:38:22 +0300

    The "Header" component is completely finished. Work on the "Body" component editing has started.

--- my-countries-website/src/pages/MainPage/components/Body/Body.js
+++ my-countries-website/src/pages/MainPage/components/Body/Body.js
@@ -2,9 +2,14 @@ import './Body.css'
 
 export const Body = () => {
     return (
-        <div className = 'body'>
-
-
+        <div className='body-background'>
+            <div className='body'>
+                <div className="body-content">
+                    <div className='content-text'>
+                        <div>Самая лучшая информация про страны всех континентов</div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
